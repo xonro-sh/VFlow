934fffc2c119aba5597de10fc84b2981d0740113
louie-001 <louie@xonro.com>
2018-01-05 09:36:27 +0800

    1.调整依赖；2.公众号功能结构

---
+++ service_no/src/main/java/com/xonro/serviceno/service/TokenService.java
@@ -0,0 +1,7 @@
+package com.xonro.serviceno.service;
+
+/**
+ * Created by louie on 2018-1-3.
+ */
+public class TokenService {
+}
---
+++ service_no/src/main/java/com/xonro/serviceno/web/WechatController.java
@@ -0,0 +1,7 @@
+package com.xonro.serviceno.web;
+
+/**
+ * Created by louie on 2018-1-3.
+ */
+public class WechatController {
+}
