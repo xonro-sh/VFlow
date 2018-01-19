c5556b57ab274e6110c661ba7ad946003fb06c18
user <1398730142@qq.com>
2018-01-19 14:18:32 +0800

    客服接口返回模型优化

--- service_no/src/main/java/com/xonro/serviceno/bean/custom/CustomServiceResult.java
+++ service_no/src/main/java/com/xonro/serviceno/bean/custom/CustomServiceResult.java
@@ -1,26 +1,30 @@
 package com.xonro.serviceno.bean.custom;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
 /**
  * @author Alex
  * @date 2018/1/10
  */
 public class CustomServiceResult {
-    private String errcode;
-    private String errmsg;
+    private String errCode;
+    private String errMsg;
 
-    public String getErrcode() {
-        return errcode;
+    @JSONField(name = "errcode")
+    public String getErrCode() {
+        return errCode;
     }
 
-    public void setErrcode(String errcode) {
-        this.errcode = errcode;
+    public void setErrCode(String errCode) {
+        this.errCode = errCode;
     }
 
-    public String getErrmsg() {
-        return errmsg;
+    @JSONField(name = "errmsg")
+    public String getErrMsg() {
+        return errMsg;
     }
 
-    public void setErrmsg(String errmsg) {
-        this.errmsg = errmsg;
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
     }
 }
