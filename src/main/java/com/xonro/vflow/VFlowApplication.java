871c3626f20945627df1d54dbd76f11063df76b0
user <1398730142@qq.com>
2018-01-29 18:23:19 +0800

    打包配置

--- src/main/java/com/xonro/vflow/VFlowApplication.java
+++ src/main/java/com/xonro/vflow/VFlowApplication.java
@@ -2,10 +2,14 @@ package com.xonro.vflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+/**
+ * @author Alex
+ * @date 2018/1/29
+ */
 @SpringBootApplication
-public class VFlowApplication {
-
+public class VFlowApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(VFlowApplication.class, args);
 	}
