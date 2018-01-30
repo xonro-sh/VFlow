66c119b5645c1a44ba95a304e8b07e9a0c9a4715
user <1398730142@qq.com>
2018-01-30 21:04:41 +0800

    微信公众号配置更换目录

--- service_no/src/main/java/com/xonro/serviceno/dao/ServiceNoConfRepository.java
+++ service_no/src/main/java/com/xonro/serviceno/dao/ServiceNoConfRepository.java
@@ -1,13 +1,18 @@
 package com.xonro.serviceno.dao;
 
 import com.xonro.serviceno.bean.config.ServiceNoConf;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Alex
  * @date 2018/1/25
  */
+@CacheConfig(cacheNames = "serviceno")
 @Repository
 public interface ServiceNoConfRepository extends JpaRepository<ServiceNoConf, Long> {
     /**
@@ -16,4 +21,7 @@ public interface ServiceNoConfRepository extends JpaRepository<ServiceNoConf, Lo
      * @return 结果模型
      */
     ServiceNoConf findById(String id);
+
+    @Cacheable
+    List<ServiceNoConf> findAll();
 }
