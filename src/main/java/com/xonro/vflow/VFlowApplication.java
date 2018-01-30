ce5e932b3c865309b43f7413d25a47c8fcaadcf6
user <1398730142@qq.com>
2018-01-30 21:08:18 +0800

    增加目录位置 缓存配置

--- src/main/java/com/xonro/vflow/VFlowApplication.java
+++ src/main/java/com/xonro/vflow/VFlowApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * @author Alex
  * @date 2018/1/29
  */
 @SpringBootApplication
+@EnableScheduling
+@EnableCaching
+@ComponentScan(basePackages = "com.xonro")
 public class VFlowApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(VFlowApplication.class, args);
