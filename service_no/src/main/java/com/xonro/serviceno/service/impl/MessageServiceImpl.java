cce07df11d00e08689940ccd40c7f967b5c18fd8
louie-001 <louie@xonro.com>
2018-02-12 09:38:48 +0800

    重构被动回复用户消息

--- service_no/src/main/java/com/xonro/serviceno/service/impl/MessageServiceImpl.java
+++ service_no/src/main/java/com/xonro/serviceno/service/impl/MessageServiceImpl.java
@@ -180,7 +180,15 @@ public class MessageServiceImpl implements MessageService{
                 //如果有效
                 if ( messages.get(0).isActive()){
                     String wechatId = "gh_5438f14fcf6f";
-                    return replyMessage(WechatEnums.MSG_TYPE_TEXT.getValue(), userOpenId, wechatId,  messages.get(0).getContent());
+
+                    OrdinaryMsg ordinaryMsg = new OrdinaryMsg();
+                    ordinaryMsg.setContent(messages.get(0).getContent());
+                    ordinaryMsg.setCreateTime((System.currentTimeMillis()/1000)+"");
+                    ordinaryMsg.setFromUserName(wechatId);
+                    ordinaryMsg.setMsgType(WechatEnums.MSG_TYPE_TEXT.getValue());
+                    ordinaryMsg.setToUserName(userOpenId);
+
+                    return replyMessage(ordinaryMsg);
                 }
             }
         }
@@ -208,31 +216,4 @@ public class MessageServiceImpl implements MessageService{
 
 
 
-    public String replyMessage(String msgType, String openId, String wechatId, String content){
-//        if (msgType.equals(WechatEnums.MSG_TYPE_TEXT.getValue())) {
-//            return ServiceNoHelper.xmlInitialUppercase(ServiceNoHelper.beanToxml(new ReplyWechatMessage(openId, wechatId, System.currentTimeMillis()/1000, msgType, content)));
-//        } else if (msgType.equals(WechatEnums.MSG_TYPE_IMAGE.getValue()) || msgType.equals(WechatEnums.MSG_TYPE_VOICE.getValue())) {
-//            String xml1 = ServiceNoHelper.xmlInitialUppercase(ServiceNoHelper.beanToxml(new ReplyWechatMessage(openId, wechatId, System.currentTimeMillis()/1000, msgType, ""))).replace("</xml>", "");
-//            String xml2 = ServiceNoHelper.getExtMessageXml(new WechatMediaMessage("2ISU_cKchVgMoCS_kC5a3Z46hcrYrN2cU3VImUIK9LKUjB0PeWa5ZjfjO3D4hAWf"),"image");
-//            return xml2==null?null:xml1.concat(xml2);
-//        } else if (msgType.equals(WechatEnums.MSG_TYPE_VIDEO.getValue())){
-//            return ServiceNoHelper.xmlInitialUppercase(ServiceNoHelper.beanToxml(new ReplyWechatMessage(openId, wechatId, System.currentTimeMillis()/1000, msgType, "")));
-//        } else if (msgType.equals(WechatEnums.MSG_TYPE_MUSIC.getValue())){
-//            String xml1 = ServiceNoHelper.xmlInitialUppercase(ServiceNoHelper.beanToxml(new ReplyWechatMessage(openId, wechatId, System.currentTimeMillis()/1000, msgType, ""))).replace("</xml>", "");
-//            String xml2 = ServiceNoHelper.getExtMessageXml(new WechatMediaMessage("测试音乐消息", "TaylorSwift", "http://url.cn/5gwf6be", "http://url.cn/5gwf6be", "kDYbKqfRkWtL1upUgPWg35poCbXpGdYkhSIyCB5jTpjMsytS8PQaaih8ML8nXgBt"),msgType);
-//            return xml2==null?null:xml1.concat(xml2);
-//        } else {
-//            WechatArticlesMessage wechatArticlesMessage = new WechatArticlesMessage("111", "测试1", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515586204344&di=07bc4720448ac27002f945959bebd443&imgtype=0&src=http%3A%2F%2Fmp3.qiyipic.com%2Fimage%2F20170727%2F96%2Fae%2Fh_1037091770_h_601_400_400.jpg", "www.baidu.com");
-//            WechatArticlesMessage wechatArticlesMessage1 = new WechatArticlesMessage("222", "测试2", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1516180979&di=76ad1dd7ac4d97ed1e7d6c85b7d25e6a&imgtype=jpg&er=1&src=http%3A%2F%2Fimg3.duitang.com%2Fuploads%2Fitem%2F201502%2F10%2F20150210221954_f4zKK.thumb.700_0.jpeg", "www.baidu.com");
-//            List<WechatArticlesMessage> wechatArticlesMessages = new ArrayList<>();
-//            wechatArticlesMessages.add(wechatArticlesMessage);
-//            wechatArticlesMessages.add(wechatArticlesMessage1);
-//            //多条图文消息
-//            String xml1 = ServiceNoHelper.xmlInitialUppercase(ServiceNoHelper.beanToxml(new ReplyWechatMessage(openId, wechatId, System.currentTimeMillis()/1000, msgType, "2"))).replace("</xml>", "");
-//            String xml2 = ServiceNoHelper.getArticlesXml(wechatArticlesMessages);
-//
-//            return xml1.concat(xml2);
-//        }
-        return "";
-    }
 }
